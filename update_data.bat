76cfb93baeaa1f6555025d5499ec52cf1992face
57yin <2106231864@qq.com>
2025-11-25 07:51:52 +0800

    Add files via upload

--- update_data.bat
+++ update_data.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 echo ============== 开始同步数据到GitHub ==============
 git add .
@@ -6,13 +5,4 @@ git commit -m "更新网易云数据"
 git push origin main
 echo ============== 数据同步完成！==============
 echo 等待1-2分钟后，Streamlit链接会自动更新最新数据
-=======
-@echo off
-echo ============== 开始同步数据到GitHub ==============
-git add .
-git commit -m "更新网易云数据"
-git push origin main
-echo ============== 数据同步完成！==============
-echo 等待1-2分钟后，Streamlit链接会自动更新最新数据
->>>>>>> 6013d9562b4a3b1f0ed7952deffda8de1afcc6f3
 pause
